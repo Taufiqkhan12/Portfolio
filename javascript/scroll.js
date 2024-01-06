736f7c8b946831e325e4bd8514643fcf61dce13c
Taufiqkhan12 <123471875+Taufiqkhan12@users.noreply.github.com>
2024-01-06 16:50:03 +0530

    Add files via upload

---
+++ javascript/scroll.js
@@ -0,0 +1,19 @@
+function scrollToAbout() {
+  const about = document.getElementById("about");
+  about.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToExpertise() {
+  const expertise = document.getElementById("expertise");
+  expertise.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToProject() {
+  const project = document.getElementById("project");
+  project.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToContact() {
+  const contact = document.getElementById("contact");
+  contact.scrollIntoView({ behavior: "smooth" });
+}
