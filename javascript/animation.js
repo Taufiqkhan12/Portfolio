938818b1317783579eabcf76270bec53fd521187
Taufiqkhan12 <123471875+Taufiqkhan12@users.noreply.github.com>
2024-01-06 16:14:43 +0530

    Add files via upload

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,1 +1,27 @@
+const mySection = document.querySelector(".expertise__card");
+const myDivs = document.querySelectorAll(".expertise__card");
 
+const option = {
+  rootMargin: "0px",
+  threshold: 0.9,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      myDivs.forEach((div) => {
+        div.classList.add("animate__fadeInUp");
+        div.classList.remove("animate__fadeOutDown");
+      });
+      observer.unobserve(mySection);
+    } else {
+      myDivs.forEach((div) => {
+        div.classList.add("animate__fadeOutDown");
+        div.classList.remove("animate__fadeInUp");
+      });
+    }
+  });
+  console.log("hello");
+}, option);
+
+observer.observe(mySection);
